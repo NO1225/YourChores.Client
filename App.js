246feb7b2598a7ddc2eaeec7c5188a5a0d84e76d
jumah safarty <safartyjumah@gmail.com>
2020-05-18 17:29:34 +0300

    Initialization

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [weather, setWeather] = useState("loading...");
+
+  const getWeather = async () => {
+    if (weather == "loading...") {
+      var res = await fetch('http://192.168.8.101:5001/weatherforecast');
+
+      var data = await res.json();
+
+      setWeather(data[0].summary);
+    }
+  }
+
+  getWeather();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{weather}</Text>
     </View>
   );
 }
